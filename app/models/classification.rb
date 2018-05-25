5156dfb631d9858f2da7177950ef45234f856715
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-25 14:53:50 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,7 +2,7 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
-  def my_all
-    all
+  def self.my_all
+    all.uniq
   end
 end
