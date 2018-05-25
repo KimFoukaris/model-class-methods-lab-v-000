f9253e16c339746051a8146313e64818a8ed4e42
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-25 15:00:56 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -5,4 +5,9 @@ class Classification < ActiveRecord::Base
   def self.my_all
     all
   end
+
+  def self.longest
+    @boat = Boat.limit(1).order("length DESC")
+    @boat.classifications
+  end
 end
